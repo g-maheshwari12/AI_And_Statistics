81c2ed0fe43ad3a4ec97afd6115d971536713e2c
Gaurav <gauravmaheshwari449@outlook.com>
2024-09-05 18:03:11 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,6 @@
+# write a python program to check whether a person is eligible or not for voting.
+age=int(input("Enter your age:"))
+if(age>=18):
+    print("ELIGIBLE")
+else:
+    print("NOT ELIGIBLE")
---
+++ 2.py
@@ -0,0 +1,8 @@
+a,b,c=8,9,10
+
+if(a>b and a>c ):
+    print('A is largest number')
+elif(b>a and b>c):
+    print('B is largest number') 
+else:
+    print('C is largest number')
